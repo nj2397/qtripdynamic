fb1d2832d857fc5bf6952a33b8ed8660ed3788c0
NIBJYOTI NATH <nibjyotin.m@yahoo.com>
2023-01-29 16:54:52 +0530

    Update adventure_details_page.js

--- frontend/modules/adventure_details_page.js
+++ frontend/modules/adventure_details_page.js
@@ -2,7 +2,6 @@ import config from "../conf/index.js";
 
 //Implementation to extract adventure ID from query params
 function getAdventureIdFromURL(search) {
-  // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Get the Adventure Id from the URL
   const params = new URLSearchParams(search);
   return params.get('adventure');
@@ -12,7 +11,6 @@ function getAdventureIdFromURL(search) {
 }
 //Implementation of fetch call with a paramterized input based on adventure ID
 async function fetchAdventureDetails(adventureId) {
-  // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Fetch the details of the adventure by making an API call
   try{
     const response = await fetch(config.backendEndpoint + `/adventures/detail?adventure=${adventureId}`);
@@ -31,7 +29,6 @@ async function fetchAdventureDetails(adventureId) {
 
 //Implementation of DOM manipulation to add adventure details to DOM
 function addAdventureDetailsToDOM(adventure) {
-  // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the details of the adventure to the HTML DOM
   document.getElementById("adventure-name").innerHTML=`${adventure.name}`;
   document.getElementById("adventure-subtitle").innerHTML=`${adventure.subtitle}`;
@@ -56,7 +53,6 @@ function addAdventureDetailsToDOM(adventure) {
 
 //Implementation of bootstrap gallery component
 function addBootstrapPhotoGallery(images) {
-  // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the bootstrap carousel to show the Adventure images
   document.getElementById("photo-gallery").innerHTML=`
     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
@@ -91,7 +87,6 @@ function addBootstrapPhotoGallery(images) {
 
 //Implementation of conditional rendering of DOM based on availability
 function conditionalRenderingOfReservationPanel(adventure) {
-  // TODO: MODULE_RESERVATIONS
   // 1. If the adventure is already reserved, display the sold-out message.
   if(adventure.available){
     document.getElementById("reservation-panel-sold-out").style.display = "none";
@@ -109,7 +104,7 @@ function conditionalRenderingOfReservationPanel(adventure) {
 
 //Implementation of reservation cost calculation based on persons
 function calculateReservationCostAndUpdateDOM(adventure, persons) {
-  // TODO: MODULE_RESERVATIONS
+
   // 1. Calculate the cost based on number of persons and update the reservation-cost field
   if(persons === 0){
     // document.getElementById("reservation-panel-available").style.display = "block";
@@ -124,7 +119,7 @@ function calculateReservationCostAndUpdateDOM(adventure, persons) {
 
 //Implementation of reservation form submission
 function captureFormSubmit(adventure) {
-  // TODO: MODULE_RESERVATIONS
+
   // 1. Capture the query details and make a POST API call using fetch() to make the reservation
   // 2. If the reservation is successful, show an alert with "Success!" and refresh the page. If the reservation fails, just show an alert with "Failed!".
   
@@ -164,7 +159,7 @@ function captureFormSubmit(adventure) {
 
 //Implementation of success banner after reservation
 function showBannerIfAlreadyReserved(adventure) {
-  // TODO: MODULE_RESERVATIONS
+  
   // 1. If user has already reserved this adventure, show the reserved-banner, else don't
   if(adventure.reserved){
     document.getElementById("reserved-banner").style.display = "block";
